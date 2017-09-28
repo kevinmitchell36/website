3e13d186d63da1ad746346dcf4bfc3ddf46ba70f
Kevin <kevingm31@gmail.com>
2017-09-28 16:14:57 -0500

    Add Bootstrap

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './views')
+
 app.get('/', function(req, res) {
-  res.send('Hello Express');
+  res.render('default', {
+    title: 'Home',
+    classname: 'home'
+  });
 });
 
-app.get('/who/:name?', function(req, res) {
-  var name = req.params.name;
-  res.send(name + 'was here');
+app.get('/about', function(req, res) {
+  res.render('default', {
+    title: 'About',
+    classname: 'about'
+  });
 });
 
-app.get('/who/:name?/:title?', function(req, res) {
-  var name = req.params.name;
-  var title = req.params.title;
-  res.send('<p>name: ' + name + '<br>title: ' + title + '</p>');
-});
 
 var server = app.listen(3000, function() {
   console.log('Listening on port 3000')
